0363efd8f4c61177e26a356701382c019b5ee564
Basma Alkerm <eng.balkerm@gmail.com>
2021-12-25 16:39:55 +0200

    phone-service front end using anguler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { phoneNumber } from './phone-number';
+
+
+class DataTablesResponse {
+  numbers: any[] = [];
+  totalCount: number = 0;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'phone-service-app';
+  dtOptions: DataTables.Settings = {};
+  numbers: phoneNumber[] = [];
+
+ constructor(private http: HttpClient) {}
+
+
+  ngOnInit(): void {
+    const that = this;
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      serverSide: true,
+      processing: true,
+      ajax: (dataTablesParameters: any, callback) => {
+        // console.log('dataTablesParameters::' , dataTablesParameters);
+        // console.log('url: ','http://localhost:8080/phones?page='+(dataTablesParameters.start/dataTablesParameters.length)+'&size='+dataTablesParameters.length )
+        that.http
+          .get<DataTablesResponse>(
+            'http://localhost:8080/phones?page='+(dataTablesParameters.start/dataTablesParameters.length)+'&size='+dataTablesParameters.length
+          ).subscribe(resp => {
+            // console.log('[ApiService] - response::', resp);
+            that.numbers = resp.numbers;
+            callback({
+              recordsTotal: resp.totalCount,
+              recordsFiltered:  resp.totalCount,
+              data: []
+            });
+          });
+      },
+      columns: [{ data: 'id' }, { data: 'customerName' }, { data: 'number' }, { data: 'country' }, { data: 'isValid' }]
+    };
+  }
 }
---
+++ src/app/phone-number.ts
@@ -0,0 +1,7 @@
+export interface phoneNumber{
+  id: number;
+  customerName: string;
+  number: string;
+  country: string;
+  valid: string;
+}
